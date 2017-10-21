ee9f05bd3c4c5f3f86b76df8c1755d011cce3bb9
Yoni Satat <yoni.satat@gmail.com>
2017-10-21 12:49:59 +0100

    added save method to class Artist

---
+++ models/artist.rb
@@ -0,0 +1,20 @@
+require_relative('../db/sql_runner.rb')
+
+class Artist
+
+  attr_reader( :name, :id)
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  def save
+    sql = "INSERT INTO artists(name)
+           VALUES($1)
+           RETURNING id"
+    values = [@name]
+    results = SqlRunner.run(sql, values)
+    @id = results.first['id'].to_i
+  end
+end
