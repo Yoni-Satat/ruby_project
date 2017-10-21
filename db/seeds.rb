e87701749d6023bd2f8b5c51eefcbc59d224be51
Yoni Satat <yoni.satat@gmail.com>
2017-10-21 12:38:05 +0100

    added Artists and Albums to seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+require_relative('../models/album.rb')
+require('pry')
+
+
+artist1 = Artist.new({
+  'name' => 'Pink Floyd'
+  })
+  artist1.save()
+
+artist2 = Artist.new({
+  'name' => 'Pet Shop Boys'
+  })
+  artist2.save()
+
+album1 = Album.new({
+  'title' => 'Dark Side of The Moon',
+  'quantity' => 10,
+  'artist_id' => artist1.id
+  })
+
+  album1.save()
+
+  album2 = Album.new({
+    'title' => 'Actually',
+    'quantity' => 10,
+    'artist_id' => artist2.id
+    })
+
+    album2.save()
