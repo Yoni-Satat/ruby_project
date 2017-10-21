1fc5534d7cf6666926cba397a3ba786a5ee7fa0c
Yoni Satat <yoni.satat@gmail.com>
2017-10-21 12:29:55 +0100

    changed class album

--- models/album.rb
+++ models/album.rb
@@ -1,11 +1,22 @@
 require_relative('../db/sql_runner.rb')
 
 class Album
-  
-  attr_reader( :id, :name)
+
+  attr_reader( :id, :title, :quantity)
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @name = options['name']
+    @title = options['title']
+    @quantity = options['quantity'].to_i
+    @artist_id = options['artist_id'].to_i
+  end
+
+  def save
+    sql = "INSERT INTO albums(name)
+           VALUES($1, $2, $3)
+           RETURNING id"
+    values = [@title, @quantity, @artist_id]
+    rssults = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
   end
 end
