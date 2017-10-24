915c7f523f67ebed02bc8bfefa671da41f2fb946
Yoni Satat <yoni.satat@gmail.com>
2017-10-24 14:07:02 +0100

    added genres table

--- db/record_store.sql
+++ db/record_store.sql
@@ -1,14 +1,21 @@
 DROP TABLE albums;
 DROP TABLE artists;
+DROP TABLE genres;
 
 CREATE TABLE artists(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
 );
 
+CREATE TABLE genres(
+  id SERIAL8 PRIMARY KEY,
+  type VARCHAR(255)
+);
+
 CREATE TABLE albums(
   id SERIAL8 PRIMARY KEY,
   title VARCHAR(255),
   quantity INT4,
-  artist_id INT8 REFERENCES artists(id)
-)
+  artist_id INT8 REFERENCES artists(id),
+  genre_id INT8 REFERENCES genres(id)
+);
