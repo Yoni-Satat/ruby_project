df4d1db6f88bc733a55c9b21259d3b9ef6fc7b83
Yoni Satat <yoni.satat@gmail.com>
2017-10-24 14:33:01 +0100

    added method to find all

--- models/genre.rb
+++ models/genre.rb
@@ -17,4 +17,11 @@ class Genre
     results = SqlRunner.run(sql, values)
     @id = results.first()['id'].to_i
   end
+
+  def self.all()
+    sql = "SELECT * FROM genres"
+    values = []
+    results = SqlRunner.run(sql, values)
+    return results.map {|genre| Genre.new(genre)}
+  end
 end
